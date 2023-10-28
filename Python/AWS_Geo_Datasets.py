786096e90d379c753e28c7ea6029f62bf80dd2d2
Amr Shalaby <amr.shalaby@mail.utoronto.ca>
2023-10-28 09:30:01 -0400

    Downloading AWS Geo Data Sets. Output file is stored in /Data/ subfolder.

--- Python/AWS_Geo_Datasets.py
+++ Python/AWS_Geo_Datasets.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 url = 'https://github.com/giswqs/aws-open-data-geo/raw/master/aws_geo_datasets.tsv'
 df = pd.read_csv(url, sep='\t')
-df.to_csv('/Users/amr/PycharmProjects/ggr473/Data/aws_geo_datasets.csv')
+df.to_csv('/Users/amr/PycharmProjects/ggr473/Data/aws_geo_datasets.csv', index_label=False, index=False)
