25c8f67de7cd12da66ed2602d5a7c4d8da04772b
Amr Shalaby <amr.shalaby@mail.utoronto.ca>
2023-11-21 10:20:11 -0500

    Data Scrubber and Ingestor is done.

--- Python/data_loader.py
+++ Python/data_loader.py
@@ -3,15 +3,28 @@ from bs4 import BeautifulSoup
 import warnings
 import pandas as pd
 import datetime
-from sqlalchemy import create_engine
-import psycopg2
+import psycopg2 as pg
+import sqlalchemy
+import os
+import io
 warnings.filterwarnings("ignore")
 
-# Create an engine instance
+# To write temp files into the Parent ./Data/ Folder to
+# to keep the Python folder clean of csv and temp files
+def get_parent_dir(directory):
+    import os
+    return os.path.dirname(directory)
 
-# Connect to PostgreSQL server
+current_dirs_parent = get_parent_dir(os.getcwd())
+parent_dir = os.path.split(os.getcwd())[0]
 
-dbConnection = alchemyEngine.connect();
+print('Working Directory: ', current_dirs_parent)
+print('Parent Directory: ', parent_dir)
+
+pg_engine = pg.connect(host='localhost', port='5433', dbname='postgres', user='postgres', password='postgres')
+# Connection String is of the form: ‘postgresql://username:password@databasehost:port/databasename’
+sqlalchemy_engine = sqlalchemy.create_engine('postgresql://postgres:postgres@localhost:5433/postgres')
+
 
 # URL from which pdfs to be downloaded
 url = "https://dd.weather.gc.ca/air_quality/aqhi/ont/observation/monthly/csv/"
@@ -29,21 +42,24 @@ i = 0
 # From all links check for CSV link and
 # if present download file
 for link in links:
-    download_link = url + link.get('href')
     if ('.csv' in link.get('href', [])):
+        download_link = url + link.get('href')
+        print('Download Link: ', download_link)
+        filename = parent_dir + '/Data/' + link.get('href')
+        print("Filename to be Written: ", filename)
         i += 1
-        #print("Downloading file: ", i, 'from link: ', link)
 
         # Get response object for link
-        #response = requests.get(download_link)
-        print('Pandas: Reading File No.: ', i, 'From Link: ',  download_link)
+        response = requests.get(download_link)
         df = pd.read_csv(download_link)
         df['last_updated'] = datetime.datetime.now()
         df['donwnload_link'] = download_link
-        df.to_sql(name='stg_monthly_aqhi_data', con=conn)
+        df['src_filename'] = filename
+        df.to_sql(name='stg_monthly_air_data', con=sqlalchemy_engine, if_exists='append', schema='stage')
         # Write content in CSV file
-        #csv = open("csv" + str(i) + ".csv", 'wb')
+        #csv = open(filename, 'wb')
         #csv.write(response.content)
         #csv.close()
 
 print("All CSV files downloaded")
+
