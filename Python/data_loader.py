ed41238edc5097d53f19f5540563cf2746ace5f2
Amr Shalaby <amr.shalaby@mail.utoronto.ca>
2023-11-22 07:24:22 -0500

    added "monthly_forecasts" web scraping

--- Python/data_loader.py
+++ Python/data_loader.py
@@ -26,7 +26,7 @@ pg_engine = pg.connect(host='localhost', port='5433', dbname='postgres', user='p
 sqlalchemy_engine = sqlalchemy.create_engine('postgresql://postgres:postgres@localhost:5433/postgres')
 
 
-def load_and_ingest(save_locally):
+def load_monthly_data(save_locally):
     # URL from which pdfs to be downloaded
     url = "https://dd.weather.gc.ca/air_quality/aqhi/ont/observation/monthly/csv/"
 
@@ -69,8 +69,59 @@ def load_and_ingest(save_locally):
                 csv.close()
     print("All CSV files downloaded")
 
-# to execute
-load_and_ingest(save_locally=False)
+def load_monthly_forecasts(save_locally):
+    # URL from which pdfs to be downloaded
+    url = "https://dd.weather.gc.ca/air_quality/aqhi/ont/forecast/monthly/csv/"
+
+    # Requests URL and get response object
+    response = requests.get(url)
+
+    # Parse text obtained
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find all hyperlinks present on webpage
+    links = soup.find_all('a')
+
+    i = 0
+    # From all links check for CSV link and
+    # if present download file
+    for link in links:
+        if ('.csv' in link.get('href', [])):
+            download_link = url + link.get('href')
+            print('Download Link: ', download_link)
+            filename = parent_dir + '/Data/Forecasts_' + link.get('href')
+            if save_locally == True:
+                print("Filename to be Written: ", filename)
+            i += 1
+
+            # Get response object for link
+            response = requests.get(download_link)
+            df = pd.read_csv(download_link, parse_dates=True)
+            df['validity date'] = pd.to_datetime(df['validity date']).dt.date
+            df.rename(columns={'validity time (UTC)':'validity_time_utc', 'cgndb code':'cgndb_code', 'amended?':'amended','validity date':'validity_date','community name':'community_name'}, inplace=True)
+            df['last_updated'] = datetime.datetime.now()
+            df['donwnload_link'] = download_link
+            df['src_filename'] = filename
+            print('Run: , ', i, 'Inserting File: ', filename, 'Into Database.')
+            df.to_sql(name='stg_monthly_forecasts', con=sqlalchemy_engine, if_exists='append', schema='stage')
+            # Write content in CSV file
+            if save_locally == True:
+                csv = open(filename, 'wb')
+                csv.write(response.content)
+                csv.close()
+    print("All CSV files downloaded")
+
+
+
+# to execute loading the monthly data into staging layer
+load_monthly_data(save_locally=False)
 ## OR to save the CSV files locally to ./Data/XXXXX.CSV  ##
 #load_and_ingest(save_locally=True)
+
+# to execute loading the monthly forecasts into the staging layer
+load_monthly_forecasts(save_locally=False)
+## OR to save the CSV files locally to ./Data/Forecasts_XXXXX.CSV  ##
+#load_monthly_forecasts(save_locally=True)
+
+
 
--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 ## Pipeline Design
 
 ## 1. Staging Layer:
-### 0. Web Scraping:
-To download the Ontario monthly air quality data from https://dd.weather.gc.ca/air_quality/aqhi/ont/observation/monthly/csv/, the Data Loader is /Python/Data_Loader.py File.
+### 0. Monthly Data Web Scraping:
+To download the Ontario monthly air quality data from https://dd.weather.gc.ca/air_quality/aqhi/ont/observation/monthly/csv/, function _load_monthly_data(boolean save_loally)_in the the Data Loader is /Python/Data_Loader.py File.
 which ingests the public data into the **first** staging table "**stg_monthly_air_data**."
 
 | Column         | Data Type        |
@@ -66,3 +66,20 @@ which ingests the public data into the **first** staging table "**stg_monthly_ai
 | donwnload_link | text             |
 | src_filename   | text             |
 
+### 1. Monthly Forecasts Data Web Scraping:
+To download the Ontario monthly air quality data from https://dd.weather.gc.ca/air_quality/aqhi/ont/forecast/model/csv/, the function _load_monthly_forecasts(boolean save_locally)_ in the Data Loader located at /Python/Data_Loader.py File.
+which ingests the public data into the **first** staging table "**stg_monthly_forecasts**" with the option to locally save the CSV files with a prefix **'FORECAST_'** to differentiate them from the actual monthly data.
+
+|      Column       | Data Type |
+|:-----------------:|:---------:|
+|       index       |  bigint   |
+|    cgndb_code     |   text    |
+|  community_name   |   text    |
+|   validity_date   |   date    |
+| validity_time_utc |   text    |
+|      period       |  bigint   |
+|       value       |  bigint   |
+|      amended      |  bigint   |
+|   last_updated    | timestamp |
+|  donwnload_link   |   text    |
+|   src_filename    |   text    |
