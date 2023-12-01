55022e3fa51ce1d5ccc566d1d1c5e25faaafcc51
Amr Shalaby <amr.shalaby@mail.utoronto.ca>
2023-12-01 09:07:57 -0500

    removed the hard-coded column names from data_transformation.py

--- Python/data_transformation.py
+++ Python/data_transformation.py
@@ -11,24 +11,18 @@ def transform_monthly_data(save_locally):
     print('*** Transposing the Monthly Air Quality Data as of: {}***'.format(a))
     sqlalchemy_engine = initialize_database()[0]
     df = pd.read_sql_table(table_name='stg_monthly_air_data', con=sqlalchemy_engine, schema='stage', parse_dates=True)
-    df_columns = ['FAFFD', 'FALIF', 'FALJI', 'FAMXK', 'FAYJG',
-                  'FAZKI', 'FBKKK', 'FBLJL', 'FBLKS', 'FCAEN', 'FCCOT', 'FCFUU', 'FCGKZ',
-                  'FCIBD', 'FCKTB', 'FCNJT', 'FCTOV', 'FCWFX', 'FCWOV', 'FCWYG', 'FDATE',
-                  'FDCHU', 'FDEGT', 'FDGED', 'FDGEJ', 'FDGEM', 'FDJFN', 'FDMOP', 'FDQBU',
-                  'FDQBX', 'FDSUS', 'FDZCP', 'FEAKO', 'FEARV', 'FEBWC', 'FEUTC', 'FEUZB',
-                  'FEVJR', 'FEVJS', 'FEVJV', 'FEVNS', 'FEVNT']
     df_out = pd.DataFrame()
-    for column in df_columns:
+    for column in df.columns:
         print('Transposing Column: ', column)
-        df2 = pd.DataFrame()
-        df2['the_date'] = df['the_date'].dt.date
-        df2['hours_utc'] = df['hours_utc']
-        df2['cgndb_id'] = str(column)
-        df2['air_quality_value'] = df[column]
-        df2['download_link'] = df['download_link']
-        df2['src_filename'] = df['src_filename']
-        df2['last_updated'] = df['last_updated']
-        df_out = pd.concat([df_out, df2])
+        df_temp = pd.DataFrame()
+        df_temp['the_date'] = df['the_date'].dt.date
+        df_temp['hours_utc'] = df['hours_utc']
+        df_temp['cgndb_id'] = str(column)
+        df_temp['air_quality_value'] = df[column]
+        df_temp['download_link'] = df['download_link']
+        df_temp['src_filename'] = df['src_filename']
+        df_temp['last_updated'] = df['last_updated']
+        df_out = pd.concat([df_out, df_temp])
     df_out.to_sql(name='stg_monthly_air_data_transpose', con=sqlalchemy_engine, if_exists='replace', schema='stage',
                   index_label=False, index=False)
 
