5f5747fe01ab8d2597a897234b670393df607cea
Amr Shalaby <amr.shalaby@mail.utoronto.ca>
2024-03-18 18:00:07 -0400

    Fixed Name of Animated HeatMap ..!!!

--- Pipeline/maps_creator.py
+++ Pipeline/maps_creator.py
@@ -30,8 +30,8 @@ def create_maps(dfs_obj, configs_obj, show: bool, add_auto_ml: bool, map_types: 
             pedestrians_group = folium.FeatureGroup(name='Pedestrians').add_to(toronto_map)
             pedestrians_heatmap_group = folium.FeatureGroup(name='Pedestrians HeatMap').add_to(toronto_map)
             # End of Feature Groups ##
-            folium.LayerControl().add_to(toronto_map)
             folium.plugins.LocateControl(auto_start=False).add_to(toronto_map)
+            folium.LayerControl().add_to(toronto_map)
             # Start of Populating the Map ##
 
             mc = f_MarkerCluster()
@@ -73,22 +73,23 @@ def create_maps(dfs_obj, configs_obj, show: bool, add_auto_ml: bool, map_types: 
 
             if add_auto_ml:
                 pass
-                #predicted_traffic_group = folium.FeatureGroup(name='Predicted Traffic').add_to(toronto_map)
-                #predicted_traffic_hm_group = folium.FeatureGroup(name='Predicted Traffic HeatMap').add_to(toronto_map)
-                #f_HeatMap(data=zip(dfs_obj.forecasts_dict['df_traffic_forecasts']['lat'], dfs_obj.forecasts_dict['df_traffic_forecasts']['lng']
-                #                 , dfs_obj.forecasts_dict['df_traffic_forecasts']['predicted_traffic']),
-                #        min_opacity=0.4, overlay=True, blur=18).add_to(predicted_traffic_hm_group)
-                #mc = f_MarkerCluster()
-                #for index, row in dfs_obj.forecasts_dict['df_traffic_forecasts'].iterrows():
-                #    folium.Marker(location=[row['lat'], row['lng']],
-                #          popup=f'Predicted Traffic: <b>{row['predicted_traffic']}</b><br>Location ID:<b>{row['location_id']}</b>'
-                #        , icon=folium.Icon(color="green", icon="flag")).add_to(mc)
-                #mc.add_to(predicted_traffic_group)
+                predicted_traffic_group = folium.FeatureGroup(name='Predicted Traffic').add_to(toronto_map)
+                predicted_traffic_hm_group = folium.FeatureGroup(name='Predicted Traffic HeatMap').add_to(toronto_map)
+                f_HeatMap(data=zip(dfs_obj.forecasts_dict['df_traffic_forecasts']['lat'], dfs_obj.forecasts_dict['df_traffic_forecasts']['lng']
+                                 , dfs_obj.forecasts_dict['df_traffic_forecasts']['predicted_traffic']),
+                        min_opacity=0.4, overlay=True, blur=18).add_to(predicted_traffic_hm_group)
+                mc = f_MarkerCluster()
+                for index, row in dfs_obj.forecasts_dict['df_traffic_forecasts'].iterrows():
+                    folium.Marker(location=[row['lat'], row['lng']],
+                          popup=f'Predicted Traffic: <b>{row['predicted_traffic']}</b><br>Location ID:<b>{row['location_id']}</b>'
+                        , icon=folium.Icon(color="green", icon="flag")).add_to(mc)
+                mc.add_to(predicted_traffic_group)
 
-            #f_HeatMapWithTime(dfs_obj.lists['traffic'], index=dfs_obj.lists['traffic'], auto_play=True, overlay=True).add_to(animated_traffic_group)
-            toronto_map.save(configs_obj.parent_dir+'/Maps/Folium_Toronto.html')
+            f_HeatMapWithTime(dfs_obj.lists['traffic'], index=dfs_obj.lists['traffic'],
+                              min_speed=5, position="topleft", auto_play=True, overlay=True
+                              , display_index=True, show=True, control=True, name='Animated Traffic').add_to(animated_traffic_group).add_to(toronto_map)
             # End of Populating the Map ##
-
+            toronto_map.save(configs_obj.parent_dir+'/Maps/Folium_Toronto.html')
             folium_end = datetime.datetime.now()
             folium_duration = (folium_end - folium_start).total_seconds()
             performance_query = f"""UPDATE public.data_model_performance_tbl
