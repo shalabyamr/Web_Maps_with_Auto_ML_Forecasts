1a3366d5bb10c3db80c1ce3f1cdd87b3d32f3730
Amr Shalaby <amr.shalaby@mail.utoronto.ca>
2024-04-07 10:34:07 -0400

    Added Geo Stations to Monthly Forecasts

--- SQL/fact_monthly_forecasts.sql
+++ SQL/fact_monthly_forecasts.sql
@@ -1,5 +1,11 @@
 DROP TABLE IF EXISTS PUBLIC.FACT_MONTHLY_FORECASTS;
 CREATE TABLE PUBLIC.FACT_MONTHLY_FORECASTS AS(
+WITH DIM_GEO_NAMES AS(
+  SELECT * FROM(
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY cgndb_id ORDER BY decision_date DESC) AS RN
+    FROM STAGE.stg_geo_names) A
+    WHERE A.RN = 1
+), MONTHLY_FORECASTS AS(
   SELECT
     cgndb_code,
     community_name,
@@ -16,4 +22,28 @@ CREATE TABLE PUBLIC.FACT_MONTHLY_FORECASTS AS(
       SELECT *
    , ROW_NUMBER() OVER(PARTITION BY "cgndb_code", "validity_date" ORDER BY validity_time_utc DESC) AS RN
       FROM STAGE.stg_monthly_forecasts
-  ) A WHERE RN=1);
+  ) A WHERE RN=1
+), EXPORT AS(
+  SELECT
+    M.cgndb_code,
+    D.latitude,
+    D.longitude,
+    D.generic_category,
+    D.geographical_name,
+    D.decision_date,
+    D.location,
+    D.province_territory,
+    M.community_name,
+    M.validity_date,
+    M.validity_time_utc,
+    M.period,
+    M.value,
+    M.amended,
+    M.download_link,
+    M.src_filename,
+    M.last_updated,
+    NOW() AT TIME ZONE 'EST' AS last_inserted
+   FROM
+   MONTHLY_FORECASTS AS M
+   INNER JOIN DIM_GEO_NAMES AS D ON UPPER(D.cgndb_id) = UPPER(M.cgndb_code)
+) SELECT * FROM EXPORT);
