bc4d089e6d91e327cf3ba278e3d73fdeed9e6d9e
Amr Shalaby <amrshalaby@live.ca>
2025-03-12 12:50:45 -0400

    Fix traffic_volume.sql due to changed column "X_id" at the source

--- SQL/traffic_volume.sql
+++ SQL/traffic_volume.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS PUBLIC.FACT_TRAFFIC_VOLUME;
 CREATE TABLE PUBLIC.FACT_TRAFFIC_VOLUME AS (
     SELECT
-    _id
+      "X_id" as "_id"
     , location_id
     , location
     , lng
@@ -9,13 +9,13 @@ CREATE TABLE PUBLIC.FACT_TRAFFIC_VOLUME AS (
     , centreline_type
     , centreline_id
     , px
-    , latest_count_date
+    , count_date as latest_count_date
     , download_link
     , src_filename
     , last_updated
     , (NOW() AT TIME ZONE 'EST') AS last_inserted
     FROM(
-      SELECT *, ROW_NUMBER() OVER(PARTITION BY location_id ORDER BY latest_count_date DESC) RN
+      SELECT *, ROW_NUMBER() OVER(PARTITION BY location_id ORDER BY count_date DESC) RN
       FROM STAGE.stg_traffic_volume
    ) A WHERE RN=1);
 
